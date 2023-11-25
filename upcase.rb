3876a881e94710c4cc05c1034a98f0fc5d13db57
MeeSup <126528516+MeeSup@users.noreply.github.com>
2023-11-25 23:50:22 +0900

    Revise the upcase file for Variables Section

---
+++ upcase.rb
@@ -0,0 +1,9 @@
+#original = "hello"
+#uppercase = original.upcase
+#puts original   # => "hello"
+#puts uppercase  # => "HELLO"
+
+original = "hello"
+original.upcase!
+puts original   # => "HELLO"
+
