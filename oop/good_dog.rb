9ed364549130f3df2b52c30b7df77fbeb3859c83
MeeSup <126528516+MeeSup@users.noreply.github.com>
2023-12-16 22:50:35 +0900

    Update attr_accessor

--- oop/good_dog.rb
+++ oop/good_dog.rb
@@ -1,24 +1,27 @@
 class GoodDog
+
+  attr_accessor :name
+
   def initialize(name)
     @name = name # @name is instance variable
   end
 
-  def name     # This was renamed from "get_name"
-    @name      # getter method (to get changed state)
-  end
+  # def name     # This was renamed from "get_name"
+  #  @name      # getter method (to get changed state)
+  # end
 
-  def name=(n)  # This was renamed from "set_name="
-    @name = n   # setter method (to change state)
-  end
+  # def name=(n)  # This was renamed from "set_name="
+  #  @name = n   # setter method (to change state)
+  # end
 
 
   def speak
-    "#{@name} says Arf!"
+    "#{name} says Arf!" #instead of @name (instance variable), its better to call getter method (renamed `name`) instead
   end
 end
 
 sparky = GoodDog.new("Sparky")
 puts sparky.speak
-puts sparky.get_name
-sparky.set_name = "Spartacus"
-puts sparky.get_name
+puts sparky.name            # => "Sparky"
+sparky.name = "Spartacus"
+puts sparky.name            # => "Spartacus"
