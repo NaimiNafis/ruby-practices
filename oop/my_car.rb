f5099a78c94d40a367eaada70037bddbee03e67a
MeeSup <126528516+MeeSup@users.noreply.github.com>
2024-02-21 08:50:24 +0800

    Add and update my_car.rb content

---
+++ oop/my_car.rb
@@ -0,0 +1,53 @@
+class MyCar
+  attr_accessor :year, :color, :model
+
+  def initialize(y, c, m)
+    @year = y
+    @color = c
+    @model = m
+    @current_speed = 0
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You push the brake and decelerate #{number} mph."
+  end
+
+  def speed_up(number)
+    @current_speed += number
+    puts "You push the gas and accelerate #{number} mph."
+  end
+
+  def shut_down
+    @current_speed = 0
+    puts "Let's park this bad boy!"
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed} mph."
+  end
+
+  def spray_paint(color)
+    self.color = color
+    puts "Your new #{color} paint job looks great!"
+  end
+
+  def info
+    puts "#{model} is a car made in #{year} with #{color} color."
+  end
+
+end
+
+lumina = MyCar.new(1997, 'chevy lumina', 'white')
+lumina.speed_up(20)
+lumina.current_speed
+lumina.speed_up(20)
+lumina.current_speed
+lumina.brake(20)
+lumina.current_speed
+lumina.brake(20)
+lumina.current_speed
+lumina.shut_down
+lumina.current_speed
+lumina.spray_paint('red')
+lumina.info
